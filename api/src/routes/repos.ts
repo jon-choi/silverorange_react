2cd7d426bd0524160b52e6c45b7915dd26448ccb
Jonathan Choi <jonchoi11@gmail.com>
2022-05-06 11:33:19 -0600

    successfully fetch api data using axios

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,7 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
+
+import jsonData from '../../data/repos.json'; // importing json repo data
 
 export const repos = Router();
 
@@ -7,6 +10,12 @@ repos.get('/', async (_: Request, res: Response) => {
 
   res.status(200);
 
+  const response = await axios.get(
+    'https://api.github.com/users/silverorange/repos' // using axios to fetch api data
+  );
+
+  const allData = [...jsonData, ...response.data]; // using spread operator and combining both data sources into one
+
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(allData);
 });
