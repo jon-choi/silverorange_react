2435904048128d2d8d281f93d941d70042ce4c83
Jonathan Choi <jonchoi11@gmail.com>
2022-05-06 13:35:42 -0600

    successfully fetch api data

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { RepoList } from './components/RepoList';
 
 import './App.css';
 
 export function App() {
   const [apiData, setApiData] = useState([]);
 
-  const baseUrl = 'http://localhost:4000/repos';
+  const baseUrl = 'http://localhost:4000';
 
   // initial load
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(baseUrl);
+        const response = await fetch(`${baseUrl}/repos`, {
+          mode: 'cors',
+        });
 
         if (response.status !== 200) {
           alert('Oops! Unable to fetch data');
@@ -25,5 +28,9 @@ export function App() {
     fetchData();
   }, []);
 
-  return <div className="App">{apiData}</div>;
+  return (
+    <div className="App">
+      {apiData === [] ? null : <RepoList apiData={apiData} />}
+    </div>
+  );
 }
--- web/src/components/RepoList.js
+++ web/src/components/RepoList.js
@@ -1,5 +1,31 @@
 import React from 'react';
 
-export function RepoList() {
-  return <div>RepoList</div>;
+export function RepoList({ apiData }) {
+  return (
+    <div>
+      <h1>List of Repositories</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Language</th>
+            <th>Number of Forks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {apiData.map((repo, index) => {
+            return (
+              <tr key={index}>
+                <td>{repo.name}</td>
+                <td>{repo.description}</td>
+                <td>{repo.language}</td>
+                <td>{repo.number_of_forks}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
