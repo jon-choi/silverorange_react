7125e9eb198a7e05a25940238f2adc327ca19716
Jonathan Choi <jonchoi11@gmail.com>
2022-05-06 13:05:03 -0600

    fetch repo data from express api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
 export function App() {
-  return <div className="App">Hello world</div>;
+  const [apiData, setApiData] = useState([]);
+
+  const baseUrl = 'http://localhost:4000/repos';
+
+  // initial load
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(baseUrl);
+
+        if (response.status !== 200) {
+          alert('Oops! Unable to fetch data');
+        } else {
+          setApiData(await response.json()); // stores fetched data in state if successful
+        }
+      } catch (error) {
+        alert('Unable to fetch data');
+      }
+    }
+    fetchData();
+  }, []);
+
+  return <div className="App">{apiData}</div>;
 }
